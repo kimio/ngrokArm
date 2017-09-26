8b1d3a9a6a52663272c6e6bbe8bf66074fac8451
Felipe Kimio Nishikaku <felipekn@ciandt.com>
2017-09-26 14:04:03 -0300

    shell install updated

--- install.sh
+++ install.sh
@@ -4,3 +4,6 @@ pkg install git
 pkg install node
 pkg install wget
 wget https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-linux-arm.zip
+npm install -g @angular/cli
+ng new angular4-linuxArm
+ng serve
